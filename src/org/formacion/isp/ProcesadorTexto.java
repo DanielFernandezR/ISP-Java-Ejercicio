378064819945562059515e35122dc5f4abe9abe2
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-04-22 17:57:14 +0200

    refactor(ProcesadorTexto): Añadir Override a métodos que usa la interfaz

--- src/org/formacion/isp/ProcesadorTexto.java
+++ src/org/formacion/isp/ProcesadorTexto.java
@@ -8,14 +8,17 @@ public class ProcesadorTexto implements ProcesarSinIdiomas, ProcesarConIdiomas{
 
 	private List<String> texto = new ArrayList<>();
 	
+	@Override
 	public void nueva (String palabra) {
 		texto.add(palabra);
 	}
 	
+	@Override
 	public String texto () {
 		return texto.stream().collect(Collectors.joining(" "));
 	}
 	
+	@Override
 	public boolean correcto (Idioma idioma) {
 		for (String palabra: texto) {
 			if (! idioma.diccionario.contains(palabra.toLowerCase())) {
